aad8a720fd8be46947f19d85c238fec86c313759
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-06-18 16:12:18 +0300

    footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,7 +1,15 @@
- function Footer() {
-    return (<div>
-        footer
-    </div>)
+import {AiFillInstagram, AiOutlineTwitter} from "react-icons/ai";
+
+function Footer() {
+    return (
+        <div className={"footer-container"}>
+            <p>2023 Solid Headphones All rights reserved</p>
+            <p className={"icons"}>
+                <AiFillInstagram/>
+                <AiOutlineTwitter/>
+            </p>
+        </div>
+    )
 }
 
 export default Footer;
