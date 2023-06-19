65d9d6e4199af6283710456ea1b442bd069001e8
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-06-19 03:28:35 +0300

    bug fixed

---
+++ dist/static/index-684ed8dd-a548f2f3.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-e98b3e3a-a11b9547.js";import"./sanity-af1b8af8.js";import"./json-inspector-fa8ed6a3.js";export{m as DocumentPaneProvider,a as default};
